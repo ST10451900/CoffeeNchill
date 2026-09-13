4a1f238911c74f5badafc715d161d00686988c58
ST10491861 <ST10491861@myemeris.edu.za>
2026-09-13 14:18:10 +0200

    feature: added GET menu items by category endpoint

--- GetMenuItemsByCategory.cs
+++ GetMenuItemsByCategory.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Azure.Data.Tables;
+using CoffeeNchill.Models;
 
 namespace CoffeeNchill
 {
-    internal class GetMenuItemsByCategory
+    public class GetMenuItemsByCategory
     {
+        private readonly ILogger _logger;
+        private const string ConnectionString = "UseDevelopmentStorage=true";
+        private const string TableName = "MenuItems";
+
+        public GetMenuItemsByCategory(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<GetMenuItemsByCategory>();
+        }
+
+        [Function("GetMenuItemsByCategory")]
+        public async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "menu/category/{category}")] HttpRequestData req,
+            string category)
+        {
+            var tableClient = new TableClient(ConnectionString, TableName);
+            await tableClient.CreateIfNotExistsAsync();
+
+            var items = new List<MenuItem>();
+
+            await foreach (var item in tableClient.QueryAsync<MenuItem>(filter: $"PartitionKey eq '{category}'"))
+            {
+                items.Add(item);
+            }
+
+            // Added error handling: Returns 404 if no items were found 
+            if (items.Count == 0)
+            {
+                var notFoundResponse = req.CreateResponse(HttpStatusCode.NotFound);
+                await notFoundResponse.WriteStringAsync($"No menu items were found for this category '{category}'.");
+                return notFoundResponse;
+            }
+
+            _logger.LogInformation($"Returning {items.Count} menu items for category {category}");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            await response.WriteAsJsonAsync(items);
+            return response;
+        }
     }
 }
