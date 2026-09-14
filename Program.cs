438208f5a7c09436d62eddc4ec519061bd689313
ST10446290-SabbirNirob <ST10446290@vcconnect.edu.za>
2026-09-14 19:54:46 +0200

    Changed and added code for docker and postman

--- Program.cs
+++ Program.cs
@@ -1,14 +1,19 @@
-using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Azure.Storage.Files.Shares;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-builder.Services
-    .AddApplicationInsightsTelemetryWorkerService()
-    .ConfigureFunctionsApplicationInsights();
+var storageConnectionString =
+    Environment.GetEnvironmentVariable("AzureWebJobsStorage")
+    ?? throw new InvalidOperationException(
+        "AzureWebJobsStorage is not configured.");
+
+builder.Services.AddSingleton(
+    new ShareServiceClient(storageConnectionString)
+);
 
 builder.Build().Run();
